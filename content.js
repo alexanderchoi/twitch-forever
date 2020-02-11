be1c47a2da25b3e4ea7cf1e2e31f1b1dbd6f6d67
alexanderchoi <achoiweb@gmail.com>
2020-02-11 14:57:17 -0800

    mvp phase 1

--- content.js
+++ content.js
@@ -1,5 +1,15 @@
-console.log("content script says hello");
+console.log("content.js executed");
 
 setInterval(function() {
-  console.log("setInterval 5 seconds");
+  console.log("set interval executed every 5s");
+  getNewStream();
 }, 5000);
+
+// import functions
+
+// set timer for 15 minutes
+// check if stream is offline
+//   if offline, get new stream
+
+// check if waystone is enabled on stream
+//   if waystone is disabled, get new stream
