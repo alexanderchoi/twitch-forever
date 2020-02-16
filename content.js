92defa2f57f7fb91a9b04e2d3684578f774bb645
alexanderchoi <alexchoiweb@gmail.com>
2020-02-16 00:10:24 -0800

    add checking offline stream and no extension use

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-console.log("content.js executed");
+// console.log("content.js executed");
 const clientid = "hevwkj5uuxx9v4e5hshn3an8k0hvl5";
 const params = {
   headers: {
@@ -11,17 +11,12 @@ const extension = `Waystone Overlay`;
 
 // Check if stream is offline or isn't using extension => get new stream
 setInterval(function() {
-  console.log(
-    "set interval executed every 5s %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
-  );
+  console.log("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+  const url = window.location.href;
+  const currentStreamerID = url.slice(url.indexOf(".tv") + 4);
+  isStreamOffline(currentStreamerID);
+  isUsingExtension(currentStreamerID);
 }, 5000);
-
-// set timer for 15 minutes
-// check if stream is offline
-//   if offline, get new stream
-
-// check if waystone is enabled on stream
-//   if waystone is disabled, get new stream
 
 function getNewStream() {
   const url = "https://api.twitch.tv/helix/streams?game_id=491403";
@@ -70,11 +65,50 @@ function redirectToStream(userID) {
     .catch(error => console.log(error));
 }
 
-export {
-  clientid,
-  params,
-  extension,
-  getNewStream,
-  findUserWithExtension,
-  redirectToStream
-};
+// export {
+//   clientid,
+//   params,
+//   extension,
+//   getNewStream,
+//   findUserWithExtension,
+//   redirectToStream
+// };
+
+function isStreamOffline(userID) {
+  // userID = "asdfasdfasf";
+  const url = `https://api.twitch.tv/helix/streams?user_login=${userID};`;
+  fetch(url, params)
+    .then(res => {
+      return res.json();
+    })
+    .then(json => {
+      console.log(json.data);
+      if (json.data.length === 0) {
+        getNewStream();
+      } else {
+        console.log("online!");
+      }
+    })
+    .catch(error => console.log(error));
+}
+
+function isUsingExtension(userID) {
+  const url = `https://api.twitch.tv/helix/users/extensions?user_id=${userID}`;
+  fetch(url, params)
+    .then(res => {
+      return res.json();
+    })
+    .then(json => {
+      const overlays = json.data.overlay;
+      let foundExtension = false;
+      for (let key in overlays) {
+        if (overlays[key].name === extension) {
+          foundExtension = true;
+        }
+      }
+      if (!foundExtension) {
+        getNewStream();
+      }
+    })
+    .catch(error => console.log(error));
+}
